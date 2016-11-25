faa2dd644cd3e506369b81fdb31e5778a19d8827
Peter Wang <peter.wang13@emc.com>
2016-11-25 06:56:59 -0500

    Fix pep8 issue

--- persistqueue/__init__.py
+++ persistqueue/__init__.py
@@ -1,15 +1,15 @@
 # coding=utf-8
 __author__ = 'Peter Wang'
-__license__ = 'apache v2.0'
+__license__ = 'Apache V2.0'
 __version__ = '0.1.0'
 
-import sys
+import sys # noqa
 if sys.version_info < (3, 0):
     from Queue import Empty, Full
 else:
     from queue import Empty, Full
 
-from .queue import Queue
+from .queue import Queue # noqa
 
 __all__ = ["Queue", "Empty", "Full", "__author__",
            "__license__", "__version__"]
