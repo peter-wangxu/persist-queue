df512ee97c965ad400110fead609387a5279d0f9
Peter Wang <peter-wangxu@users.noreply.github.com>
2017-10-09 22:06:19 -0500

    [GH-22]Fix issue when using :memory: as path (#23)
    When using the in-memory database, a directory named
    :memory: was created accidentally thus a disk based
    queue was setup.
    
    This patch will fix it and stores the data in the memory
    directly.

--- persistqueue/__init__.py
+++ persistqueue/__init__.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 __author__ = 'Peter Wang'
 __license__ = 'BSD License'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 from .exceptions import Empty, Full  # noqa
 from .pdict import PDict  # noqa
