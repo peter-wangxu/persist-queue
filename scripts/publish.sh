1fca826797c79688af7eeb8313622cb6fce0d3d3
Peter Wang <wangxu198709@gmail.com>
2021-04-07 19:22:09 +0800

    remove dirty release files

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,6 +6,7 @@ if [[ "$NAME" != "persist-queue" ]];then
     echo "must run this in project root"
     exit 1
 fi
+rm -rf ./build/*.* ./dist/*.*
 python setup.py build sdist
 python setup.py build bdist_wheel
 twine check ${BASE_DIR}/dist/*.tar.gz
