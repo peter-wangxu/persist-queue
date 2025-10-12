ffe64db06d7d7711de8e9b2690b660d5372cc5cd
Peter Wang <wangxu198709@gmail.com>
2025-10-12 19:18:17 +0800

    Fix miss py.typed

--- persistqueue/__init__.py
+++ persistqueue/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Peter Wang'
 __license__ = 'BSD'
-__version__ = '1.1.0-alpha'
+__version__ = '1.1.0-beta'
 
 # Relative imports assuming the current package structure
 from .exceptions import Empty, Full  # noqa: F401
